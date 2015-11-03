76d02564a821fe7a65f9af80afdb8d341690984c
Mohammed Abdur Rafay <rafay.engg@gmail.com>
2015-11-03 13:36:01 +0530

    First commit

---
+++ app/scripts/directives/stk-watchlist-panel.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkWatchlistPanel
+ * @description
+ * # stkWatchlistPanel
+ */
+angular.module('stockDogApp')
+  // [1] Register Directive and inject dependencies
+  .directive('stkWatchlistPanel', function ($location, $modal, $routeParams, WatchlistService) {
+    return {
+      templateUrl: 'views/templates/watchlist-panel.html',
+      restrict: 'E',
+      scope: {},
+      link: function ($scope) {
+        // [2] Initialize variable
+        $scope.watchlist = {};
+
+        var addListModal = $modal({
+          scope: $scope,
+          template: 'views/templates/addlist-modal.html',
+          show: false
+        });
+
+        // [3] Bind model from service to this scope
+        $scope.watchlists = WatchlistService.query();
+
+        // [4] Display addlist modal
+        $scope.showModal = function () {
+          addListModal.$promise.then(addListModal.show);
+        };
+
+        // [5] Create a new list from fields in Modal
+        $scope.createList = function () {
+          WatchlistService.save($scope.watchlist);
+          addListModal.hide();
+          $scope.watchlist = {};
+        };
+
+        // [6] Delete desired list and redirect to home
+        $scope.deleteList = function (list) {
+          WatchlistService.remove(list);
+          $location.path('/');
+        };
+
+        // [7]
+        $scope.currentList = $routeParams.listId;
+        $scope.gotoList = function (listId) {
+          $location.path('watchlist/' + listId);
+        };
+      }
+    };
+  });
